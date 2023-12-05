f8a0df790dc29c36e2a252ec6f5cd79df600e2fb
Eren Nevin <eren.m.nevin@gmail.com>
2023-12-05 18:25:22 +0300

    Initial

---
+++ src/config.ts
@@ -0,0 +1,30 @@
+import { Token } from '@uniswap/sdk-core'
+import { FeeAmount } from '@uniswap/v3-sdk'
+import { USDC_TOKEN, WETH_TOKEN } from './constants'
+
+// Inputs that configure this example to run
+export interface ExampleConfig {
+  rpc: {
+    mainnet: string
+  }
+  tokens: {
+    in: Token
+    amountIn: number
+    out: Token
+    poolFee: number
+  }
+}
+
+// Example Configuration
+
+export const CurrentConfig: ExampleConfig = {
+  rpc: {
+    mainnet: 'https://mainnet.infura.io/v3/989a33ef7f114f45b58346df500b9917',
+  },
+  tokens: {
+    in: USDC_TOKEN,
+    amountIn: 1000,
+    out: WETH_TOKEN,
+    poolFee: FeeAmount.MEDIUM,
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { quote } from "./quote";
+
+console.log("Quoting")
+
